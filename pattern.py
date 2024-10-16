aba61a24f6fd5d1537808c36bc3fdac72fc42e04
Nazar <parkasevychnazarij@gmail.com>
2024-10-16 17:02:06 +0300

    projects 1

---
+++ pattern.py
@@ -0,0 +1,16 @@
+import turtle as t
+import random
+t.speed(1000)
+t.bgcolor("black")
+t.pensize(1)
+colors = ["blue","yellow"]
+
+for i in range(100):
+    t.lt(145)
+    for j in range(5):
+        n = random.choice(colors)
+        t.color(n)
+        t.fd(250)
+        t.lt(150)
+        t.hideturtle()
+t.done()
